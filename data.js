f7d6125ad89f029708122d3ada262319c562db2a
TarunAndEehita <tarunandeehita@gmail.com>
2024-08-15 11:27:33 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,19 @@
+const tableAssignments = {
+    1: ['Amar', 'Gurinder', 'Hina', 'Paras', 'Pooja', 'Richa', 'Samaye', 'Sufie', 'Tushar', 'Vignesh'],
+    2: ['Cedin', 'Dannel', 'Daniel', 'Dylan', 'Elizabeth', 'Kat', 'Matthew', 'Vimi', 'Zurah'],
+    3: ['Alice', 'Ansh', 'Arvin', 'Aseem', 'Daphne', 'Divya', 'Evans', 'Jessie', 'Raya', 'Vincent'],
+    4: ['Ashish Kapoor', 'Lavanya', 'Nidhi Gupta', 'Nikita', 'Nitin', 'Palak', 'Tirthesha', 'Varun', 'Varun Asija', 'Virna Argüelles'],
+    5: ['Abir', 'Dibyo', 'Dilawar', 'Isha', 'Mani', 'Samairah', 'Shweta', 'Surya', 'Varun Kapoor'],
+    6: ['Anmol', 'Kerim', 'Keshav', 'Michael', 'Moksh', 'Naveena Persaud', 'Niomi', 'Nikolai', 'Ryan', 'Surya Verma'],
+    7: ['Aneesh', 'Ankur', 'Anushka', 'Arjun', 'Hritik', 'Sarthak', 'Swayam', 'Yash'],
+    8: ['Anurag', 'Deepa', 'Mukesh', 'Naresh Kalra', 'Rajeev Sood', 'Sangeeta', 'Surbhi', 'Sunita Babbar'],
+    9: ['Anu Aggarwal', 'Anu Gulati', 'Anu Vohra', 'Kamal', 'Puneet', 'Rajni', 'Ravi', 'Sandhya', 'Vimal', 'Yogesh'],
+    10: ['Arvind', 'Krishna Bhuri', 'Krishn Bhuri', 'Mr Sabarwal', 'Neeraj Verma', 'Nirmal Sabarwal', 'Ranjev', 'Rekha', 'Sarita Verma', 'Seema'],
+    11: ['Gajender', 'Indu', 'Kashmiri Lal', 'Raj', 'Sonia', 'Subhash', 'Urmil Sharda'],
+    12: ['Aamodini', 'Ajay', 'Amit', 'Anju', 'Arjun', 'Geetika', 'Raj Rani', 'Ridhi', 'Ritika', 'Sarita'],
+    13: ['Ashwani', 'Jasbir Kaur', 'Mohini Suri', 'Nisha', 'Parveen', 'Ramesh Mehra', 'Savita', 'Shyam', 'TP Singh'],
+    14: ['Manya', 'Neeraj Chhabra', 'Nina', 'Sonia Chhabra', 'Sunita', 'Surinder', 'Tanish Chhabra'],
+    15: ['Aman', 'Archna', 'Dharmendra', 'Inder', 'Madhu', 'Shelly', 'Shubra', 'Shubra Mom', 'Sunil', 'Vandna'],
+    16: ['Dr. Rajesh', 'Hemant', 'Pooja', 'Rajni', 'Renu', 'Sandeep Satiza and Family', 'Satish Puri', 'Vijay'],
+    17: ['Ashok', 'Kuldeep', 'Malkesh', 'Manju', 'Nidhi Gaur', 'Poonam Kapila', 'Prabh', 'Prem', 'Surinder']
+};
