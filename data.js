eb0904cc9a708d87911cb9f3f2063082da7e49eb
TarunAndEehita <tarunandeehita@gmail.com>
2024-08-16 15:47:07 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 const tableAssignments = {
-    1: ['Amar', 'Gurinder', 'Hina', 'Paras', 'Pooja', 'Richa', 'Samaye', 'Sufie', 'Tushar', 'Vignesh'],
+    1: ['Amar', 'Gurinder', 'Hina', 'Paras Gupta', 'Pooja', 'Richa', 'Samaye', 'Sufie', 'Tushar Babbar', 'Vignesh'],
     2: ['Cedin', 'Dannel', 'Daniel', 'Dylan', 'Elizabeth', 'Kat', 'Matthew', 'Vimi', 'Zurah'],
     3: ['Alice', 'Ansh', 'Arvin', 'Aseem', 'Daphne', 'Divya', 'Evans', 'Jessie', 'Raya', 'Vincent'],
     4: ['Ashish Kapoor', 'Lavanya', 'Nidhi Gupta', 'Nikita', 'Nitin', 'Palak', 'Tirthesha', 'Varun', 'Varun Asija', 'Virna Argüelles'],
     5: ['Abir', 'Dibyo', 'Dilawar', 'Isha', 'Mani', 'Samairah', 'Shweta', 'Surya', 'Varun Kapoor'],
-    6: ['Anmol', 'Kerim', 'Keshav', 'Michael', 'Moksh', 'Naveena Persaud', 'Niomi', 'Nikolai', 'Ryan', 'Surya Verma'],
+    6: ['Anmol', 'Kerim', 'Keshav', 'Michael', 'Moksh Bhuri', 'Naveena Persaud', 'Niomi', 'Nikolai', 'Ryan', 'Surya Verma'],
     7: ['Aneesh', 'Ankur', 'Anushka', 'Arjun', 'Hritik', 'Sarthak', 'Swayam', 'Yash'],
     8: ['Anurag', 'Deepa', 'Mukesh', 'Naresh Kalra', 'Rajeev Sood', 'Sangeeta', 'Surbhi', 'Sunita Babbar'],
     9: ['Anu Aggarwal', 'Anu Gulati', 'Anu Vohra', 'Kamal', 'Puneet', 'Rajni', 'Ravi', 'Sandhya', 'Vimal', 'Yogesh'],
